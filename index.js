3275e507b9d87aea3908f6cf80e79c58ba13a3dc
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2019-11-29 05:01:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newarray = []
+  for ( var i = 0; i < musicians.length; i++) {
+    newarray.push(musicians[i] + " plays " + instruments[i])
+  }
+  return newarray
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  var newarray = []
+  while (i < facts.length) {
+    newarray.push(facts[i] + "!!!")
+    i++
+  }
+  return newarray
+}
+
+function iLoveTheBeatles(number) {
+  var i = 0;
+  var newarray = [];
+    if (number < 15) {
+      var i = 0;
+      function incrementVariable() {
+        i++
+        return i
+      }
+      do {
+        newarray.push("I love the Beatles!");
+      } while (incrementVariable() <= number);
+    }
+    else {
+      newarray.push("I love the Beatles!");
+    }
+  return newarray
+}
